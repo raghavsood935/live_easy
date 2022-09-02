ff980c2505532aa7a770d6fb0d9098ccf7ac006d
Raghav <93931430+raghavsood935@users.noreply.github.com>
2022-09-02 14:33:37 +0530

    Final Changes

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: ProfileSelection(),
+      home: SelectLanguage(),
     );
   }
 }
